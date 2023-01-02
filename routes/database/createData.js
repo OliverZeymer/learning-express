9e14f33afe5fbeae571a60cb23724dc25141bcde
OtakuBoy420 <U28282@rts-365.dk>
2023-01-02 10:36:31 +0100

    add more informative responses

--- routes/database/createData.js
+++ routes/database/createData.js
@@ -1,54 +1,31 @@
-import useDB from "../../server.js";
-import capitalize from "../../functions/capitalize.js";
+import useDB from "../../server.js"
+import capitalize from "../../functions/capitalize.js"
 export default async function createData(request, response) {
-  const requiredKeys = ["name", "country"];
-  const isArray = Array.isArray(request.body);
-  const { collection, client } = await useDB(
-    capitalize(request.params.resource)
-  );
-
-  const singleObject = [Object.assign({}, request.body)];
-  const data = isArray ? request.body : singleObject;
-
-  const isValid = data.every((item) => {
-    const hasRequriedKeys = requiredKeys.every((key) =>
-      item.hasOwnProperty(key)
-    );
-    return hasRequriedKeys;
-  });
-
-  if (isValid === false) {
-    response.status(422).json({
-      message: "Name & Country are required",
-    });
-    return;
-  }
-
+  const isArray = Array.isArray(request.body)
+  const { collection, client } = await useDB(capitalize(request.params.resource))
+  const singleObject = [Object.assign({}, request.body)]
+  const data = isArray ? request.body : singleObject
   try {
     if (isArray === false) {
-      const result = await collection.insertOne(data[0]);
+      const result = await collection.insertOne(data[0])
       response
         .status(201)
         .json({
-          message:
-            "Customer with ID: " + result.insertedId + " succesfully created!",
+          message: capitalize(request.params.resource).slice(0, -1) + " with ID: " + result.insertedId + " succesfully created!",
         })
-        .end();
-      return;
+        .end()
+      return
     }
-    const result = await collection.insertMany(data);
+    const result = await collection.insertMany(data)
     response
       .status(201)
       .json({
-        message:
-          "Customer with ID: " +
-          result.insertedIds[0] +
-          " succesfully created!",
+        message: capitalize(request.params.resource).slice(0, -1) + " with ID: " + result.insertedIds[0] + " succesfully created!",
       })
-      .end();
+      .end()
   } catch (error) {
-    response.status(500).json({ message: "Something went wrong" });
+    response.status(500).json({ message: "Something went wrong" })
   }
 
-  client.close();
+  client.close()
 }
--- routes/database/deleteData.js
+++ routes/database/deleteData.js
@@ -1,21 +1,21 @@
-import useDB from "../../server.js";
-import { ObjectId } from "mongodb";
-import capitalize from "../../functions/capitalize.js";
+import useDB from "../../server.js"
+import { ObjectId } from "mongodb"
+import capitalize from "../../functions/capitalize.js"
 export default async function deleteData(request, response) {
-  const { collection } = await useDB(capitalize(request.params.resource));
-  const id = request.params.id;
+  const { collection } = await useDB(capitalize(request.params.resource))
+  const id = request.params.id
   if (id) {
     if (!ObjectId.isValid(id)) {
       response.status(400).json({
         message: "Invalid ID",
-      });
-      return;
+      })
+      return
     }
-    collection.deleteOne({ _id: ObjectId(id) });
+    collection.deleteOne({ _id: ObjectId(id) })
     response.json({
-      message: id + " has been succesfully deleted!",
-    });
+      message: capitalize(request.params.resource).slice(0, -1) + " with " + id + " has been succesfully deleted!",
+    })
   } else {
-    response.send("You need to provide an id");
+    response.send("You need to provide an id")
   }
 }
